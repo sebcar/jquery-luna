acefa9917fc88b2e01796cb66289e2e36ce91f47
Sebastián Caraballo <sebcarpr@yahoo.com.ar>
2016-02-20 20:18:48 -0300

    Add event onStatusChange

--- js/jquery-luna.js
+++ js/jquery-luna.js
@@ -15,10 +15,10 @@
 </div>
 */
 (function ( $ ) {
- 
+
     $.fn.luna = function(options) {
         var settings = $.extend({
-                      
+
         }, options ),
 		htmlOutput = '<div class="luna-container luna">\
 			<div class="luna-controls">\
@@ -37,127 +37,158 @@
 				Your browser does not support the audio element. Please update your browser.\
 			</audio>\
 		</div>',
-		$this = this, player = {}, btnPlayPause = {}, btnNext = {},
-		infoCurrent = {}, infoTotal = {}, indexPlaying = 0,
-		progressBar = {}, vol = {};		
+        //declare vars
+		$this = this, $player = {}, $btnPlayPause = {}, $btnNext = {},
+		$infoCurrent = {}, $infoTotal = {}, indexPlaying = 0,
+		$progressBar = {}, $vol = {}, $msg = {}, playerStatus = '';
 		$this.append(htmlOutput);
-		btnPlay = $('.luna-play');
-		btnPause = $('.luna-pause');
-		btnNext = $('.luna-next');
-		btnPrev = $('.luna-prev');
-		infoCurrent = $('.luna-time-current');
-		infoTotal = $('.luna-time-total');
-		progressBar = $('.luna-progress-bar');
-		vol = $('.luna-volume');
-		player = document.getElementById('luna-audio-tag');
-		player.playPause = function(){			
-			if (player.paused === true){
-				player.play();
-				btnPlay.trigger('focus');	
-			}else if (player.paused === false){
-				player.pause();
-				btnPause.trigger('focus');	
+        //assign vars
+		$btnPlay = $('.luna-play');
+		$btnPause = $('.luna-pause');
+		$btnNext = $('.luna-next');
+		$btnPrev = $('.luna-prev');
+		$infoCurrent = $('.luna-time-current');
+		$infoTotal = $('.luna-time-total');
+		$progressBar = $('.luna-progress-bar');
+		$vol = $('.luna-volume');
+        $msg = $('.luna-status-msg');
+		$player = document.getElementById('luna-audio-tag');
+        //play-pause
+		$player.playPause = function(e){
+			if ($player.paused === true){
+				$player.play();
+				$btnPlay.trigger('focus');
+                setEventStatusChange('playing');
+
+			}else if ($player.paused === false){
+				$player.pause();
+				$btnPause.trigger('focus');
+                setEventStatusChange('paused');
 			}
-		}			
-		
+		}
+        //next song
 		function nextSong(){
 			if (indexPlaying < settings.songs.length - 1){
-				indexPlaying++;				
+				indexPlaying++;
 			}else{
 				indexPlaying = 0;
-			}			
+			}
 		}
-		
+        //formating time for bar
 		function formatTime(sec) {
 			min = Math.floor(sec / 60);
 			min = (min >= 10) ? min : "0" + min;
 			sec = Math.floor(sec % 60);
 			sec = (sec >= 10) ? sec : "0" + sec;
 			return min + ":" + sec;
-		}		
-		  
+		}
+        //check progress
 		function checkProgress(c,t){
 			var p = parseInt((c * 100) / t);
-			progressBar.slider( "option", "value", p );
+			$progressBar.slider( "option", "value", p );
 		}
-		
+        //set progress
 		function setProgress(t,p){
-			player.currentTime = parseInt( (p*t) / 100 );
+			$player.currentTime = parseInt( (p*t) / 100 );
 		}
-		
-		player.anotherSong = function(songPath){
-			infoTotal.text('0:00');
-			player.src = songPath;
-			btnPlay.trigger('focus');	
-			player.play();
+        //play another song
+		$player.anotherSong = function(songPath){
+			$infoTotal.text('0:00');
+			$player.src = songPath;
+			$btnPlay.trigger('focus');
+			$player.play();
+            setEventStatusChange('changing_song');
 		}
-		
-		btnPlay.on('click', function() {
-			player.playPause();				
+        //events
+		$btnPlay.on('click', function() {
+			$player.playPause();
 		} );
-		btnPause.on('click', function() {
-			player.playPause();		
+		$btnPause.on('click', function() {
+			$player.playPause();
 		} );
-		btnNext.on('click', function(){
+		$btnNext.on('click', function(){
 			nextSong();
-			player.anotherSong(settings.songs[indexPlaying]);			
+			$player.anotherSong(settings.songs[indexPlaying]);
 		});
-		btnPrev.on('click', function(){
+		$btnPrev.on('click', function(){
 			if (indexPlaying === 0){
 				indexPlaying = settings.songs.length - 1;
 			}else{
 				indexPlaying--;
 			}
-			player.anotherSong(settings.songs[indexPlaying]);
+			$player.anotherSong(settings.songs[indexPlaying]);
 		} );
-		
-		player.onended = function(){			
+
+		$player.onended = function(){
 			nextSong();
-			player.anotherSong(settings.songs[indexPlaying]);
+			$player.anotherSong(settings.songs[indexPlaying]);
+            setEventStatusChange('song_finished');
 		}
-		
+
 		function timeUpdate(){
-			player.ontimeupdate = function () {
-				infoCurrent.text( formatTime( player.currentTime ) );	
-				checkProgress(player.currentTime, player.duration);
-			}			
+			$player.ontimeupdate = function () {
+				$infoCurrent.text( formatTime( $player.currentTime ) );
+				checkProgress($player.currentTime, $player.duration);
+			}
 		}
 		function noTimeUpdate(){
-			player.ontimeupdate = function () {
-			
+			$player.ontimeupdate = function () {
+
 			}
 		}
 		timeUpdate();
-		
-		
-		player.onloadedmetadata = function(){
-			infoTotal.text( formatTime( player.duration ) );
+
+
+		$player.onloadedmetadata = function(){
+			$infoTotal.text( formatTime( $player.duration ) );
+            setEventStatusChange('metadata_loaded');
 		}
-		
+
 		//loading
-		player.src = settings.songs[0];			
-		infoCurrent.text( formatTime( player.currentTime ) );	
-		progressBar.slider({
+		$player.src = settings.songs[0];
+		$infoCurrent.text( formatTime( $player.currentTime ) );
+		$progressBar.slider({
 			animate: 'fast',
-			slide:	function(event, ui){								
-				setProgress(player.duration, ui.value);
+			slide:	function(event, ui){
+				setProgress($player.duration, ui.value);
+                setEventStatusChange('changing_progress');
 			}
 		});
-		progressBar.on('mousedown', function() {
+		$progressBar.on('mousedown', function() {
 			noTimeUpdate();
 		} );
-		progressBar.on('mouseup', function(){
+		$progressBar.on('mouseup', function(){
 			timeUpdate();
 		}  );
-		player.volume = 0.75;
-		vol.slider({
+		$player.$volume = 0.75;
+		$vol.slider({
 			animate: 'fast',
 			slide: function(event, ui) {
-				player.volume = ui.value / 100;				
+				$player.$volume = ui.value / 100;
+                setEventStatusChange('volumen_changing');
 			},
 			value: 75
 		});
+
+        $player.oncanplay = function(){
+            setEventStatusChange('ready_to_play');
+        }
+
+        //PUBLIC EVENTS
+
+        //get status
+        $this.getStatus = function(){
+            return status;
+        }
+        //set public event
+        function setEventStatusChange(s){
+            status = s;
+            if (options.onStatusChange !== undefined){
+                options.onStatusChange(status);
+            }
+        }
+
         return this;
     };
- 
+
 }( jQuery ));
